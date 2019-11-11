b2eea03223691e3831e2db3271d6894d8746d40c
Joe <burtonjae@hotmail.co.uk>
2019-11-11 14:48:47 +0000

    Read templates from config

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ module.exports = app => {
   app.on('issue_comment.created', async context => {
     let command = context.payload.comment.body;
     if (command.startsWith("?")) {
+      const config = await context.config("earthbot.yml", {});
       command = command.substring(1);
       const args = command.split(" ");
-      if (args[0] === "template") {
+      if (args[0] === "template" && config.templates) {
         if (args.length < 2) return;
-        if (args[1] === "64bitjava") {
-          const issueComment = context.issue({ body: "This issue is commonly caused by not having 64-bit Java on your system. Please run `java -version` in `cmd` to check if you are using 32-bit. If you are, use [this link](https://javadl.oracle.com/webapps/download/AutoDL?BundleId=239858_230deb18db3e4014bb8e3e8324f81b43) to install 64-bit Java, then try running the Enchantment Cracker again." });
+        if (args[1] in config.templates) {
+          const issueComment = context.issue({ body: config.templates[args[1]] });
           return context.github.issues.createComment(issueComment);
         }
       }
